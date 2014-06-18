6da9dac5fbd500bcd1ed655dd43a099fdc85972c
Alexander Popov <alexanderpopov1@gmail.com>
2014-06-18 17:32:59 -0400

    Add openssl to rubies
    Added a libssl to required software list and ensured rubies are built with sll flag

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 sudo apt-get update
-sudo apt-get install -y git make mongodb openjdk-7-jre-headless g++ isc-dhcp-server ipxe tftp tftpd curl
+sudo apt-get install -y git make mongodb openjdk-7-jre-headless g++ isc-dhcp-server ipxe tftp tftpd curl zlib1g-dev libssl-dev
 
 #Configure tftpd
 [ ! -d /tftpboot ] && sudo mkdir /tftpboot
@@ -31,29 +31,33 @@ sudo cp /vagrant/default /tftpboot/pxelinux.cfg/default
 #Configure DHCPd
 sudo cp /vagrant/dhcpd.conf /etc/dhcp/dhcpd.conf
 
-git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
-echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bash_profile
-echo 'eval "$(rbenv init -)"' >> ~/.bash_profile
-source ~/.bash_profile
-
-git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
-
 #Get Hanlon
 sudo mkdir /opt/hanlon/
 sudo chmod -R 777 /opt/hanlon/
 sudo chown -R nobody /opt/hanlon/
 
+#Get Hanlon
 cd /opt/hanlon/
 git clone https://github.com/csc/Hanlon.git .
 
+#Install rbenv
+git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bash_profile
+echo 'eval "$(rbenv init -)"' >> ~/.bash_profile
+source ~/.bash_profile
+
+#Install ruby-build tool
+git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+./~/.rbenv/plugins/ruby-build/install.sh
+
 #Install rubies
-rbenv install 1.9.3-p547
+ruby-build 1.9.3-p547 ~/.rbenv/versions/1.9.3-p547 --with-openssl-dir=/usr/local
 rbenv global 1.9.3-p547
 gem install bundler trinidad
 rbenv rehash
 bundle install
 
-rbenv install jruby-1.7.12
+ruby-build jruby-1.7.12 ~/.rbenv/versions/jruby-1.7.12 --with-openssl-dir=/usr/local
 rbenv global jruby-1.7.12
 gem install bundler trinidad
 rbenv rehash
